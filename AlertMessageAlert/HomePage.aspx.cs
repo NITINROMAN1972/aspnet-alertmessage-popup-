702b828b3808a3e39eb175939c904213ffab79dc
NITINROMAN1972 <romannitin5@gmail.com>
2023-12-27 16:29:02 +0530

    initial code

---
+++ AlertMessageAlert/HomePage.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class HomePage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        AlertMessage("Hello, this is an example of Aert Message !");
+    }
+
+    public void AlertMessage(string message)
+    {
+        ScriptManager.RegisterStartupScript(this, this.GetType(), Guid.NewGuid().ToString(), " alert('" + message + "');", true);
+    }
+}
